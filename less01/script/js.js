0fa63850230a3e4bf98f5c4538b277c4d36b4b11
Mariana <mariana.kuznets09@gmail.com>
2026-05-07 11:46:16 +0300

    Add files via upload

---
+++ less01/script/js.js
@@ -0,0 +1,73 @@
+// document.write("Hello JS");
+// console.log("Hello World!");
+// console.info("Hello World!");
+// console.error("Hello World!");
+// console.warn("Hello World!");
+
+// var MyFirstVar;
+// MyFirstVar = 10;
+// console.log(MyFirstVar);
+// // ==
+// var MySecondVar='23';
+// console.log(MySecondVar + MySecondVar);
+// console.log(MySecondVar - MyFirstVar);
+//
+// var int= 10;
+// var float= 12.5;
+// var bool=true; //false
+// var str ='6';
+// var a;
+// a = null;
+// console.log(a);
+
+// let b  = 10.,a = b * 2;
+
+// let a;
+// a = 10;
+// console.log(a);
+
+// let a = 10;
+// a = 20;
+// console.log(a);
+//
+//
+// const PI = 3.14;
+
+let a = 10, b= 3;
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+console.log(a%b);
+console.log(a**b);
+
+// n = 2+2;
+// b = a+10;
+//
+// a = a + 2;
+// a /= 2;
+
+// // інкремент
+// a ++;
+// // декремент
+// a --;
+//
+// // суфіксна
+// --a;
+// // префіксна
+// ++a;
+
+// let a =2, b;
+// b = ++a;
+// cosole.log(a);
+
+// let a = '3', b = '5';
+// console.log((a-0)+(b-0));
+// console.log(Number(a) - Number(b));
+//
+//
+// // Інтерполяція
+//
+// let name ="Іван", age = 18;
+// console.log("Привіт" + name + "Тобі" + age + "років");
+// console.log(`Привіт ${name}. Тобі ${age} років!! ${(a-0)+(b-0)};
